67b4e73fcad533510e3087b3e32ce1aca9822a58
Sean Malan <sean.malan@developersinstitute.co.nz>
2022-08-29 17:59:03 +1200

    added a post route with a celebrate model for validation of posts.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const JobModel = require("./Models/JobModel")
+const { errors, Segments } = require ("celebrate")
+const Joi = require("joi")
+
 
 app.use(express.json())
 app.use(cors())
 
+
+app.post("/jobs", celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required(),
+  job: Joi.string().required(),
+  clientName: Joi.string().required(),
+  clientPhoneNumber: Joi.number().required(),
+  jobAddress: Joi.string().required(),
+  description: Joi.string().required(),
+  jobStatus: Joi.string().required(),
+  jobNotes: Joi.string().required(),
+  createdDate: Joi.date().required()
+  })
+}), async (request, response, next) => {
+  try{
+    const job = new JobModel(request.body)
+  await job.save()
+  response.status(201).send(job)
+  } catch (error){
+    next(error)
+  }
+  
+})
 
 
 
@@ -20,5 +46,6 @@ app.get("/jobs/:id", async (request, response) => {
   response.send(job).status(200)
 })
 
+app.use(errors())
 
 module.exports = app
