542388072d17018ca9e6109dba33e46861501fbf
Sean Malan <sean.malan@developersinstitute.co.nz>
2022-08-29 16:38:21 +1200

    added a get all jobs route

--- src/app.js
+++ src/app.js
@@ -2,3 +2,10 @@ const express = require("express")
 const cors = require("cors")
 const app = express
 
+app.request(express.json())
+
+
+app.get("/jobs", async (request, response) => {
+  const jobs = await JobModel.find({})
+  response.send(jobs).status(200)
+})
