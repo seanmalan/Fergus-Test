25e6b709094b82b5c85188e60ac35d9e273f7e2a
Sean Malan <sean.malan@developersinstitute.co.nz>
2022-08-29 17:35:40 +1200

    added the get by id route

--- src/app.js
+++ src/app.js
@@ -4,11 +4,20 @@ const app = express()
 const JobModel = require("./Models/JobModel")
 
 app.use(express.json())
+app.use(cors())
+
+
 
 
 app.get("/jobs", async (request, response) => {
   const jobs = await JobModel.find({})
   response.send(jobs).status(200)
+})
+
+app.get("/jobs/:id", async (request, response) => {
+  const {id} = request.params
+  const job = await JobModel.findById(id)
+  response.send(job).status(200)
 })
 
 
