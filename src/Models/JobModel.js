4f11f2cbc4eecf4b9ec6fe016da0e22c6fa3bda2
Sean Malan <sean.malan@developersinstitute.co.nz>
2022-08-29 17:58:21 +1200

    update model with createdDate

--- src/Models/JobModel.js
+++ src/Models/JobModel.js
@@ -5,10 +5,11 @@ const JobSchema = mongoose.Schema({
   job: { type: String, required: true },
   clientName: { type: String, required: true },
   clientPhoneNumber: { type: Number, required: true },
-  addressjobAddress: { type: String, required: true },
+  jobAddress: { type: String, required: true },
   description: { type: String, required: true },
   jobStatus: { type: String, required: true },
   jobNotes: { type: String, required: true },
+  createdDate: {type:Date, required:true}
 });
 
 module.exports = mongoose.model("Job", JobSchema);
